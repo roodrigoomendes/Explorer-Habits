9c6d76292e8b2822f35a9977dd68cda76e2e3641
Rodrigo Mendes <roodrigoomendessilva@gmail.com>
2023-01-22 19:01:06 -0300

    Finalizando logica da Aplicação

---
+++ script.js
@@ -0,0 +1,36 @@
+const form = document.querySelector('#form-habits')
+const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')
+
+button.addEventListener('click', add)
+form.addEventListener('change', save)
+
+
+function add(){
+
+    const today = new Date()
+    .toLocaleDateString('pt-br')
+    .slice(0,-5)
+
+    const dayExists = nlwSetup.dayExists(today)
+
+    if(dayExists){
+        alert(' ❌ O dia ja existe ')
+        return
+    } 
+    
+    nlwSetup.addDay(today)
+    alert('✅ Adicionado com sucesso ')
+
+}
+
+function save(){
+    localStorage.setItem('DATAhabits', 
+    JSON.stringify(nlwSetup.data)) 
+}
+
+    
+const data = JSON.parse(localStorage.getItem('DATAhabits')) || {}
+
+nlwSetup.setData(data)
+nlwSetup.load()
